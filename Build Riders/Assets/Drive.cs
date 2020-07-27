f95fe616e18a8e70de37ce246f2cc474b258ed9c
Guz9ka <rodder2017@yandex.ru>
2020-07-27 23:16:38 +0300

    cerf
    Новая версия юнити

---
+++ Build Riders/Assets/Drive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public Rigidbody carRb = new Rigidbody();
+    int car_pos = 2;
+    int kozla = 2;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") && car_pos != 1)
+        {
+            car_pos = 
+            //carRb.AddForce()
+        }
+        Debug.Log(kozla);
+    }
+}
