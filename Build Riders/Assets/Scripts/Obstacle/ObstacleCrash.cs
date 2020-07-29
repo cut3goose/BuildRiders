b2ac54c016c874e79e9392cc2c532f087d3aaa7b
Guz9ka <rodder2017@yandex.ru>
2020-07-29 15:53:59 +0300

    Препятствия, ресурсы, поворот машины
    Неплохая такая основа, и губы ничего

---
+++ Build Riders/Assets/Scripts/Obstacle/ObstacleCrash.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ObstacleCrash : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            GameObject Car = GameObject.Find("Car");
+            Car.GetComponent<Drive>().enabled = false;
+            //Car.GetComponent<Rigidbody>().AddExplosionForce();  можно добавить взрыв после столкновения, чтобы машинка падала на бок
+        }
+    }
+}
