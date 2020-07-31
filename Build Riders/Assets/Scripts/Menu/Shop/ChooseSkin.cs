9a72ac931f64394ca95c9d475a464048aa5a03c7
Guz9ka <rodder2017@yandex.ru>
2020-07-31 17:25:48 +0300

    коммит с скелетом магаза

---
+++ Build Riders/Assets/Scripts/Menu/Shop/ChooseSkin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseSkin : MonoBehaviour
+{
+    public RawImage skinPreview;
+    public Texture[] skinList = new Texture[8];
+
+    void Start()
+    {
+        //тут должно быть обращение в память, чтобы понять какой скин у игрока сейчас
+        //skinPreview = UserInfo.CurrentSkinNumber;
+        skinPreview.texture = skinList[0];
+    }
+
+    public void ChooseSkinButton(int skinNumber)
+    {
+        skinPreview.texture = skinList[skinNumber - 1];
+    }
+}
