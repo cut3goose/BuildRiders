554d5dcc26426b796bb1b82936cf50c3866d521b
Guz9ka <rodder2017@yandex.ru>
2020-08-01 15:15:34 +0300

    начато переписывание скриптов в геймменджер

--- Build Riders/Assets/Scripts/Car/ObstacleCrash.cs
+++ Build Riders/Assets/Scripts/Car/ObstacleCrash.cs
@@ -3,36 +3,30 @@ using System.Collections.Generic;
 
 public class ObstacleCrash : MonoBehaviour
 {
-    public GameObject Car;
+    public GameObject _GameManager;
+    public GameObject car;
 
     public float switchScreenDelay = 4.0f;
     public GameObject deathScreenCanvas;
-    
+
+    private void Start()
+    {
+        car = gameObject;
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Player")
         {
-            Car.GetComponent<Drive>().enabled = false;
-            Car.transform.position = Vector3.zero;
+            car.GetComponent<Drive>().enabled = false;
 
             Invoke("chooseScreen", switchScreenDelay);
             //Car.GetComponent<Rigidbody>().AddExplosionForce();  можно добавить взрыв после столкновения, чтобы машинка падала на бок
         }
     }
+
+    void SendDeathInfo()
+    {
+        _GameManager.
+    }
     
-    void chooseScreen()
-    {
-        openDeathScreen();
-    }
-
-    void openDeathScreen()
-    {
-        deathScreenCanvas.SetActive(true);
-        Invoke("DestroyObstacle", 1f);
-    }
-
-    void DestroyObstacle()
-    {
-        Destroy(gameObject);
-    }
 }
